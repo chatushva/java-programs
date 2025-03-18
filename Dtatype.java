83c5965d93b6f71e438da84096fbe8ddb28bb3ef
chatushva <immidisettichatushva@gmail.com>
2025-03-18 22:12:21 +0530

    Create Dtatype.java

---
+++ Dtatype.java
@@ -0,0 +1,23 @@
+//code to print all static values of datatypes in java
+class Datatype
+{
+    static byte b;
+    static short s;
+    static char c;
+    static boolean bl;
+    static int i;
+    static float f;
+    static long l;
+    static double d;
+    public class void main(String args[])
+    {
+        System.out.println("byte "+b);
+        System.out.println("short "+s);
+        System.out.println("char "+c);
+        System.out.println("boolean "+bl);
+        System.out.println("int "+i);
+        System.out.println("float "+f);
+        System.out.println("long "+l);
+        System.out.println("double "+d);
+    }
+}
