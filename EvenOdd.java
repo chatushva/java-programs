eeb8b9adc1d5d167f4fd2ae1041439aebb5b5df4
chatushva <immidisettichatushva@gmail.com>
2025-03-21 10:23:04 +0530

    EvenOdd.java

---
+++ EvenOdd.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Even
+{
+    public static void main(String args[])
+    {
+        System.out.println("enter any number");
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        if(n%2==0)
+        {
+            System.out.println("is even");
+        }
+        else
+        {
+            System.out.println("is odd");
+        }
+    }
+}
