9733201f16268ec503ea1d9fa092a9638d78f1f7
chatushva <immidisettichatushva@gmail.com>
2025-04-22 16:18:21 +0530

    Update Armatrong.java

--- Armatrong.java
+++ Armatrong.java
@@ -1,33 +1,36 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+import java.util.*;
+class Aramstrong
 {
     public static void main(String args[])
     {
-        int digits=0,sum=0;
-        System.out.println("enter any number");
         Scanner sc=new Scanner(System.in);
+        System.out.println("enter any number:");
         int n=sc.nextInt();
         int temp=n;
+        int sum=0,digit=0;
         while(n>0)
         {
             n=n/10;
-            digits++;
-            System.out.println(digits);
+            digit++;
         }
-        System.out.println("no of digit is  "+digits);
+        System.out.println("digits:"+digit);
         n=temp;
         while(n>0)
         {
-            int value=n%10;
-            sum += Math.pow(value,digits);
-            n/=10;
+            int res=n%10;
+            sum+=Math.pow(res,digit);
+            n=n/10;
         }
-         System.out.println(sum);
-         if(sum==temp)
-         {
-             System.out.println("given number is armstrong");
-         }
-         else
-         {
-             System.out.println("not an armstrong");
-         }
+        System.out.println("sum value is"+sum);
+        if(sum==temp)
+        {
+           System.out.println("amstrong");
+        }
+        else
+        {
+         System.out.println(" not amstrong");
+        }
     }
 }
