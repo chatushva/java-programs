e7c2eaaff52837dd1f4d3327db58fc62312fab63
chatushva <immidisettichatushva@gmail.com>
2025-04-25 13:20:01 +0530

    GCD.java

---
+++ GCD.java
@@ -0,0 +1,31 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+import java.util.*;
+class GCD
+{
+    public int gcd(int n1,int n2)
+    {
+         for(int i=Math.min(n1,n2);i>=1;i--)
+         {
+             if(n1%i==0&&n2%i==0)
+             {
+                 int gcd=i;
+                 System.out.println(gcd);
+                 break;
+             }
+         }
+         return 0;
+         
+    }
+    public static void main(String args[])
+    {
+        GCD g=new GCD();
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the n1:");
+        int n1=sc.nextInt();
+        System.out.println("enter the n2:");
+        int n2=sc.nextInt();
+       int result= g.gcd(n1,n2);
+       System.out.println(result);
+    }
+}
